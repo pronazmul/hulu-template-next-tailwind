a61a25d895272f051b147e0fe99375771b7d33a5
pronazmul <developernazmul@gmail.com>
2021-11-05 21:33:17 +0600

    react-flip-move animation done

--- components/Results.js
+++ components/Results.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Thumbnail from './Thumbnail'
+import FlipMove from 'react-flip-move'
 
 function Results({results}) {
     return (
-        <div className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
             {results.map(result=>(
                 <Thumbnail key={result.id} result={result}/>
             ))}
-        </div>
+        </FlipMove>
     )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import{ThumbUpIcon} from '@heroicons/react/outline'
+import { forwardRef } from 'react'
 
-function Thumbnail({result}) {
+const Thumbnail = forwardRef(({result}, ref)=> {
     const BASE_URL = "https://image.tmdb.org/t/p/original"
     return (
-        <div className='group cursor-pointer transition duration-200 sm:hover:scale-105 hover:z-50'>
+        <div ref={ref} className='group cursor-pointer transition duration-200 sm:hover:scale-105 hover:z-50'>
             <Image
             layout='responsive'
             src={BASE_URL+result.backdrop_path||BASE_URL+result.poster_path}
@@ -22,6 +23,6 @@ function Thumbnail({result}) {
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail
