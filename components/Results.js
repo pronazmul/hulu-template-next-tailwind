34fe4bbe4cb0934fa54560f2270d600ca62b5a01
pronazmul <developernazmul@gmail.com>
2021-11-05 21:22:28 +0600

    Hulu 2.0 practice project done

---
+++ components/Results.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Thumbnail from './Thumbnail'
+
+function Results({results}) {
+    return (
+        <div className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+            {results.map(result=>(
+                <Thumbnail key={result.id} result={result}/>
+            ))}
+        </div>
+    )
+}
+
+export default Results
